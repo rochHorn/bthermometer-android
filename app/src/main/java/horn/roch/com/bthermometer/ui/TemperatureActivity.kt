1ad6bddddf8932bfe4588a942b1d25a484e5f63f
unknown <user.name>
2016-02-02 23:32:44 +0100

    Create project base packages, added first activity

---
+++ app/src/main/java/horn/roch/com/bthermometer/ui/TemperatureActivity.kt
@@ -0,0 +1,23 @@
+package horn.roch.com.bthermometer.ui
+
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+
+import horn.roch.com.bthermometer.R
+
+class TemperatureActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_temperature)
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+
+        val fab = findViewById(R.id.fab) as FloatingActionButton
+        fab.setOnClickListener { view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show() }
+    }
+
+}
